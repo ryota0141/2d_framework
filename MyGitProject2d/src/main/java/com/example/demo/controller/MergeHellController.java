3c5624fd6c13be1eb43a53d2b051eb0ee434120f
S0525 <S0525@21S0525>
2023-11-30 09:45:03 +0900

    お先

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+
+		kadaiC();
+	}
+
+		static void kadaiC() {
+	int a = 50;
+	int b = 75;
+	int c = a+b;
+
+	System.out.println(c);
+		}
+}
